dc7f22889c71d35797a6d545fe942ecde42e02fa
Ravi Sisodia <ravi.sisodia.3010@gmail.com>
2024-01-29 18:47:55 +0530

    added ios & android builds with capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.simplifyem',
   appName: 'SimplifyEM',
   webDir: 'www',
   server: {
